4720d0c5d6b1de8939fbf3dbf471abeabc6fee98
sunsadforever <sunsad11314167@gmail.com>
2026-06-23 14:48:14 +0800

    Update settings.py

--- web_project/settings.py
+++ web_project/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'django-insecure-o*x5oau$md5y663mmv3cb!6$b=&do%c%0wla^2-7$at)xa*&1l
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://web-project-hx73.onrender.com",
+    "http://localhost:8000",
+]
 
 # Application definition
 
